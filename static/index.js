8aaf090ad56e886f275721695e097cf0f0a741d3
galileogenerobin <galileogenerobin@gmail.com>
2022-03-06 23:34:58 +0800

    Updated the Index page
    1. Revamped the UI in the index page to resemble Google-style design
    2. Used Bootstrap's input-group system instead of the grid system to group and align each line in the order form
    3. Customized / overrode some of the default Bootstrap properties
    4. Removed "delivery address"; Delivery will be handled in another page
    5. Added an event handler in the JS file to prevent the Enter key from submitting the form
    6. TODO: Change the select elements to radio buttons or toggle buttons

--- static/index.js
+++ static/index.js
@@ -16,6 +16,7 @@ const DELIVERY_FEE = 10;
 const ORDER_TEXT_COLOR = 'rgb(40, 40, 40)';
 
 $(document).ready(function () {
+    // Store elements into corresponding variables
     deliveryModeElement = $('#delivery-mode');
     containerTypeElement = $('#container-type');
     quantityElement = $('#quantity');
@@ -25,6 +26,7 @@ $(document).ready(function () {
     totalPriceElement = $('#total');
     totalPriceText = $('#total-text');
     
+    // The succeeding lines set up the event listeners for our form elements
     containerTypeElement.on('change', function() {
         containerType = this.value;
         
@@ -34,6 +36,7 @@ $(document).ready(function () {
     quantityElement.change(function() {
         quantity = this.value;
 
+        // If user enters a negative value, reset to 0
         if (quantity < 0) {
             this.value = 0;
             quantity = this.value;
@@ -67,6 +70,7 @@ $(document).ready(function () {
     });
 });
 
+// Compute the prices and update on screen
 function updatePrices(quantity, deliveryMode, swapNew) {
     let pricePerUnit = 0;
 
@@ -89,8 +93,13 @@ function updatePrices(quantity, deliveryMode, swapNew) {
         }
     }
 
-    pricePerUnitText.html(`PhP ${pricePerUnit.toFixed(2)}`);
-    pricePerUnitElement.value = pricePerUnit;
-    totalPriceText.html(`PhP ${(pricePerUnit * quantity).toFixed(2)}`);
-    totalPriceElement.value = pricePerUnit * quantity;
+    pricePerUnitElement.val(`PhP ${pricePerUnit.toFixed(2)}`);
+    totalPriceElement.val(`PhP ${(pricePerUnit * quantity).toFixed(2)}`);
 }
+
+// Prevent Enter key from submitting the form
+// This is basically preventing the Enter key from doing anything
+// Source: https://stackoverflow.com/questions/895171/prevent-users-from-submitting-a-form-by-hitting-enter#:~:text=Disallow%20enter%20key%20anywhere&text=on(%22keydown%22%2C%20%22,be%20checked%20on%20the%20key%20.
+$(document).on("keydown", ":input:not(textarea)", function(event) {
+    return event.key != "Enter";
+});
